1ead9a8e9bb9b736891d28a28f2e14f7f0b0e330
MaxDovgopoliy <73337901+MaxDovgopoliy@users.noreply.github.com>
2022-08-17 11:13:40 +0300

    Delete src directory

--- src/main/java/com/service/backoffice/exeption/TariffNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.service.backoffice.exeption;
-
-public class TariffNotFoundException extends  RuntimeException{
-
-    public TariffNotFoundException(String massage) {
-        super(massage);
-    }
-}
