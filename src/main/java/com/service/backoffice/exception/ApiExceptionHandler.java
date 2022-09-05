ab5badd412f15b700298cde51d0fd00b121d7960
mdovhopolyi <lenoms22@gmail.com>
2022-09-05 12:08:17 +0300

    GGO-LVIV-T-110 [Dovhopolyi] add validation exception handling

--- src/main/java/com/service/backoffice/exception/ApiExceptionHandler.java
+++ src/main/java/com/service/backoffice/exception/ApiExceptionHandler.java
@@ -1,6 +1,10 @@
 package com.service.backoffice.exception;
 
+import java.util.stream.Collectors;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -16,6 +20,22 @@ public class ApiExceptionHandler {
                         .errorId(exception.getException().getId())
                         .errorName(exception.getException().name())
                         .message(exception.getException().getMessage())
+                        .path(request.getDescription(false).substring(4))
+                        .build());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse> catchNotValidException(MethodArgumentNotValidException e,
+                                                              WebRequest request) {
+        String errorMessages = e.getBindingResult().getAllErrors().stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.joining("; "));
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ApiResponse.of()
+                        .errorId(404)
+                        .errorName("Not Valid")
+                        .message(errorMessages)
                         .path(request.getDescription(false).substring(4))
                         .build());
     }
